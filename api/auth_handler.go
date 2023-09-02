0c75974fbec6a9d0a2c7c8aa86ae426209f15c38
Yogesh K <yogeshcodes@zohomail.in>
2023-09-02 17:34:47 +0530

    Write tests for authentication handler
    Indent

--- api/auth_handler.go
+++ api/auth_handler.go
@@ -3,6 +3,7 @@ package api
 import (
 	"errors"
 	"fmt"
+	"net/http"
 	"os"
 	"time"
 
@@ -27,10 +28,21 @@ type AuthResponse struct{
     Token string        `json:"token"`
 }
 
+type genericResp struct {
+    Type  string `json:"type"`
+    Msg   string `json:"msg"`
+}
+
 func NewAuthHandler(userstore db.UserStore ) *AuthHandler{
     return &AuthHandler{
         userStore: userstore,
     }
+}
+func invalidCredentialsError(c *fiber.Ctx) error{
+    return c.Status(http.StatusBadRequest).JSON(genericResp{
+        Type: "error",
+        Msg:  "invalid credentials",
+    })
 }
 
 func (h *AuthHandler)HandleAuthenticate(c *fiber.Ctx) error{
@@ -41,12 +53,12 @@ func (h *AuthHandler)HandleAuthenticate(c *fiber.Ctx) error{
     user, err := h.userStore.GetUserByEmail(c.Context(), params.Email)
     if err != nil{
         if errors.Is(err, mongo.ErrNoDocuments){
-            return fmt.Errorf("invalid credentials")
+            return invalidCredentialsError(c)
         }
         return err
     }
     if !types.IsPasswordValid(user.EncryptedPassword, params.Password){
-        return fmt.Errorf("invalid credentials")
+        return invalidCredentialsError(c)
     }
     resp := AuthResponse{
         User: user,
---
+++ api/auth_handler_test.go
@@ -0,0 +1,105 @@
+package api
+
+import (
+    "bytes"
+    "context"
+    "encoding/json"
+    "net/http"
+    "net/http/httptest"
+    "reflect"
+    "testing"
+
+    "github.com/codebyyogesh/hotel-booking-service/db"
+    "github.com/codebyyogesh/hotel-booking-service/types"
+    "github.com/gofiber/fiber/v2"
+)
+
+func insertTestUser(t *testing.T, userStore db.UserStore) *types.User {
+    user, err := types.NewUserFromParams(types.CreateUserParams{
+        Email:     "some@foo.com",
+        FirstName: "Some",
+        LastName:  "Foo",
+        Password:  "mybestsecurepassword",
+    })
+    if err != nil {
+        t.Fatal(err)
+    }
+    _, err = userStore.InsertUser(context.Background(), user)
+    if err != nil {
+        t.Fatal(err)
+    }
+    return user
+}
+func TestAuthenticateWithWrongPassword(t *testing.T) {
+    tdb := setup(t)
+    defer tdb.teardown(t)
+    insertTestUser(t, tdb.UserStore)
+
+    app := fiber.New()
+    authHandler := NewAuthHandler(tdb.UserStore)
+    app.Post("/auth", authHandler.HandleAuthenticate)
+
+    params := AuthParams{
+        Email:    "some@foo.com",
+        Password: "mybestsecurepasswordwrongpassword",
+    }
+    b, _ := json.Marshal(params)
+    req := httptest.NewRequest("POST", "/auth", bytes.NewReader(b))
+    req.Header.Add("Content-Type", "application/json")
+    resp, err := app.Test(req)
+    if err != nil {
+        t.Fatal(err)
+    }
+    if resp.StatusCode != http.StatusBadRequest {
+        t.Fatalf("expected status code 400 but got %d", resp.StatusCode)
+    }
+
+    var genResp genericResp
+    if err := json.NewDecoder(resp.Body).Decode(&genResp); err != nil {
+        t.Error(err)
+    }
+    if genResp.Type != "error" {
+        t.Fatalf("expected genResponse type to be error but got %s", genResp.Type)
+    }
+    if genResp.Msg != "invalid credentials" {
+        t.Fatalf("expected genResponse msg to be invalid credentials but got %s", genResp.Msg)
+    }
+}
+func TestAuthenticateSuccess(t *testing.T) {
+    tdb := setup(t)
+    defer tdb.teardown(t)
+    insertedUser := insertTestUser(t, tdb.UserStore)
+
+    app := fiber.New()
+    authHandler := NewAuthHandler(tdb.UserStore)
+    app.Post("/auth", authHandler.HandleAuthenticate)
+
+    params := AuthParams{
+        Email:    "some@foo.com",
+        Password: "mybestsecurepassword",
+    }
+    b, _ := json.Marshal(params)
+    req := httptest.NewRequest("POST", "/auth", bytes.NewReader(b))
+    req.Header.Add("Content-Type", "application/json")
+    resp, err := app.Test(req)
+    if err != nil {
+        t.Fatal(err)
+    }
+    if resp.StatusCode != http.StatusOK {
+        t.Fatalf("expected status code %d, got %d", http.StatusOK, resp.StatusCode)
+    }
+
+    var authResp AuthResponse
+    if err := json.NewDecoder(resp.Body).Decode(&authResp); err != nil {
+        t.Error(err)
+    }
+    if authResp.Token == "" {
+        t.Fatalf("expected the JWT token to be present in the auth response")
+    }
+    // Because we do not return encrypted password in any of the JSON response, thus we need to
+    // set it to null string when comparing with authResp.User
+    insertedUser.EncryptedPassword = ""
+    if !reflect.DeepEqual(authResp.User, insertedUser) {
+        t.Fatalf("expected the user to be %v, got %v", insertedUser, authResp.User)
+    }
+}
